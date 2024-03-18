94d26552274120b720c43989d9bda1705d13098a
Dee Torna <tornadavinder@gmail.com>
2024-03-18 12:47:43 +0000

    dropdown fixed

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-
 class Category(models.Model):
-
     class Meta:
         verbose_name_plural = 'Categories'
         
@@ -23,7 +21,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True) 
+    image_url = models.CharField(max_length=2000, null=True, blank=True)  
 
     def __str__(self):
         return self.name
