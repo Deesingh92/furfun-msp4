bd39a1fdff261110eebad418f05be3ca0bbec5b1
Dee Torna <tornadavinder@gmail.com>
2024-03-18 11:26:05 +0000

    dropdown fixed

--- shop/views.py
+++ shop/views.py
@@ -7,32 +7,35 @@ def shop(request):
 
     products = Product.objects.all()
     all_categories = Category.objects.all()
-    categories = all_categories  # Initialize categories with all categories by default
+    categories = all_categories 
 
     query = None
     sort = None
     direction = None
 
     if request.GET:
+        # Sorting
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'name_lower'
                 products = products.annotate(name_lower=Lower('name'))
-            if sortkey == 'category':
+            elif sortkey == 'category':
                 sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
             selected_categories = request.GET.getlist('category')
-            products = products.filter(category__name__in=selected_categories)
-            # Update categories to only include selected categories
-            categories = all_categories.filter(name__in=selected_categories)
+            if selected_categories:
+                products = products.filter(category__name__in=selected_categories)
+                categories = all_categories.filter(name__in=selected_categories)
+            else:
+                categories = None
 
     current_sorting = f'{sort}_{direction}'
 
@@ -42,7 +45,8 @@ def shop(request):
         'current_sorting': current_sorting,
     }
 
-    return render(request, 'includes/header.html', context)
+    return render(request, 'shop/shop.html', context)
+
 
 def product_detail(request, product_id):
     """A view to display details of a specific product"""
