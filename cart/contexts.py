d8e9cd4b0019a4d4c57521e4145f9fb19466b860
Dee Torna <tornadavinder@gmail.com>
2024-04-17 09:23:49 +0000

    toasts completed

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,15 +1,27 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
-from .models import Order, CartItem
+from .models import Order, CartItem, Cart
 from shop.models import Product
 from .utils import get_or_create_cart
 
-@login_required
 def cart_contents(request):
-    cart = get_or_create_cart(request)
-    cart_items = cart.cart_items.all()
-    cart_total = sum(item.get_item_total() for item in cart_items)
-    return {
-        'cart_items': cart_items,
-        'cart_total': cart_total,
-    }
+    cart = request.session.get('cart', {})
+    cart_items = []
+    
+    if isinstance(cart, dict) and 'cart_id' in cart:
+        cart_id = cart['cart_id']
+        try:
+            cart_model = Cart.objects.get(pk=cart_id)
+            cart_items = cart_model.cart_items.all()
+        except Cart.DoesNotExist:
+            pass
+    elif isinstance(cart, dict) and 'user_id' in cart:  
+        user_id = cart['user_id']
+        try:
+            user = User.objects.get(pk=user_id)
+            cart_model = user.cart
+            cart_items = cart_model.cart_items.all()
+        except User.DoesNotExist:
+            pass
+    
+    return {'cart_items': cart_items}
