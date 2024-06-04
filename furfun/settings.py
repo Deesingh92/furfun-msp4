83d6e73558c4902a63e663eb232109e1a6daec24
Dee Torna <tornadavinder@gmail.com>
2024-06-04 10:16:42 +0000

    deployment

--- furfun/settings.py
+++ furfun/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['furfun-dbbf5a9a33bb.herokuapp.com', 'localhost', '8000-deesingh92-furfunmsp4-i69n50ed687.ws-eu114.gitpod.io']
 
